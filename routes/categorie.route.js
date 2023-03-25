0109922ade3ea8f7470b54a2cd9ac96ac2bc619d
emna.abbes5 <emna.abbes5@gmail.com>
2023-03-26 00:09:28 +0100

    new commit

--- routes/categorie.route.js
+++ routes/categorie.route.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 // Créer une instance de categorie.
 const Categorie = require('../models/categorie');
+const auth = require( "../middleware/auth.js"); 
 // afficher la liste des categories.
-router.get('/', async (req, res,) => {
+router.get('/', auth ,async (req, res,) => {
     try {
         const cat = await Categorie.find();
         res.status(200).json(cat);
